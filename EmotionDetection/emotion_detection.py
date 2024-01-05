69ca40a6139a4f29c3e7f14a322309d651eea612
Catarina Esposito Mendes <espositc@uoguelph.ca>
2024-01-05 12:07:11 -0500

    Edits for Final Project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    header = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+
+        dict_emotions = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+        }
+
+        dominant_emotion = max(dict_emotions, key=dict_emotions.get)
+
+        dict_emotions['dominant_emotion'] = dominant_emotion
+    elif response.status_code == 400:
+        dict_emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
+    return dict_emotions
